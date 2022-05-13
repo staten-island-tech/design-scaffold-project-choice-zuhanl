ab9240b11564306ed3edd25500065ff4bd0f390d
ZuhanL <ZUhanL@nycstudents.net>
2022-05-13 13:40:40 -0400

    5/13

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,18 @@
 import './style.css';
-import { gsap } from "gsap";
 
+const DOMSelectors = {
+    buttontheme: document.getElementById("btn69"),
+};
+  
+
+DOMSelectors.buttontheme.addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  });
 
 
