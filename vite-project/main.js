46523a09d8e143fb21b6f89a84913115d1634c4a
ZuhanL <ZUhanL@nycstudents.net>
2022-05-18 13:54:34 -0400

    5/18

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,10 +1,19 @@
 import './style.css';
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline({scrollTrigger:".pic",delay: "0.3"});
+
+tl.from("h1", {opacity: 0, duration: 0.3, ease: "ease-out"});
+tl.from(".home", {opacity: 0, duration: 0.3, ease: "ease-out"});
+tl.from(".pic", {opacity: 0, duration: 0.3, ease: "ease-out"});
+
+
 
 const DOMSelectors = {
     buttontheme: document.getElementById("btn69"),
 };
   
-
 DOMSelectors.buttontheme.addEventListener("click", function () {
     if (document.body.classList.contains("light")) {
       document.body.classList.add("dark");
